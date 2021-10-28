1b76b5174b68bf5b8a866663a31b120c0c9d4c30
Rudis Muiznieks <rudis@sitosis.com>
2021-10-28 13:42:47 -0500

    keymap tweaking, added macro layer

--- keyboards/mitosis/keymaps/rudism_dv/keymap.c
+++ keyboards/mitosis/keymaps/rudism_dv/keymap.c
@@ -9,7 +9,8 @@ enum mitosis_layers
 {
     _STD,
     _NUM,
-    _FN
+    _FN,
+    _MACRO
 };
 
 enum custom_keycodes
@@ -17,7 +18,6 @@ enum custom_keycodes
     PLPYTOG = SAFE_RANGE,
     PLPYHLD
 };
-
 
 //Mousekeys
 #define MOUSEKEY_DELAY 300
@@ -39,9 +39,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     * |--------+--------+--------+--------+--------||--------+--------+--------+--------+--------|
     * | Z      | X      | C      | V      | B      || N      | M      | ,      | .      | /      |
     * '--------+--------+--------+--------+--------||--------+--------+--------+--------+--------'
-    *          | MOUSE2 | MOUSE1 | LCTRL  | LGUI   || RGUI   | LALT   | MY PC  | PAUSE  |
+    *          | MOUSE2 | MOUSE1 | LCTRL  | LGUI   || RGUI   | LALT   | PRTSCR | VOL+   |
     *          |--------+--------+--------+--------||--------+--------+--------+--------|
-    *          | SCRHLD | ESC    | LSHIFT | ENTER  || SPACE  | RSHIFT | BCKSPC | PRTSCR |
+    *          | SCRHLD | ESC    | LSHIFT | ENTER  || SPACE  | RSHIFT | BCKSPC | VOL-   |
     *          '-----------------------------------''-----------------------------------'
     */
     [_STD] = LAYOUT( /* Standard; as compatible with dvorak and qwerty as possible
@@ -49,8 +49,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
       KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,
       KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,
-               KC_BTN2, KC_BTN1, KC_LCTL, KC_LGUI, KC_RGUI, KC_LALT, KC_MYCM, KC_PAUS,
-               PLPYHLD, KC_CAPS, KC_LSFT, LT_FN,   LT_NUM,  KC_RSFT, KC_BSPC, KC_PSCR
+               KC_BTN2, KC_BTN1, KC_LCTL, KC_LGUI, KC_RGUI, KC_LALT, KC_PSCR, KC_VOLU,
+               PLPYHLD, KC_CAPS, KC_LSFT, LT_FN,   LT_NUM,  KC_RSFT, KC_BSPC, KC_VOLD
     ),
 
    /* Number layout, for data entry and programming purposes (Dvorak result in parens)
@@ -61,17 +61,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     * |--------+--------+--------+--------+--------||--------+--------+--------+--------+--------|
     * | F1     | F2     | F3     | F4     | F5     || F6     | F7     | F8     | F9     | F10    |
     * '--------+--------+--------+--------+--------||--------+--------+--------+--------+--------'
-    *          | F11    |        |        |        ||        |        |        | F12    |
+    *          |        |        |        |        ||        |        |        |        |
     *          |--------+--------+--------+--------||--------+--------+--------+--------|
-    *          | HOME   |        |        |        ||        |        |        | MENU   |
+    *          |        |        |        |        ||        |        |        |        |
     *          '-----------------------------------''-----------------------------------'
     */
     [_NUM] = LAYOUT( /* Number layout along the home row for maximum speed*/
         KC_CALC, _______, _______, KC_MINS, KC_EQL,  KC_RBRC, KC_QUOT, KC_GRV,  KC_LBRC, KC_BSLS,
         KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,
         KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,
-                 KC_F11,  _______, _______, _______, _______, _______, _______, KC_F12,
-                 KC_HOME, _______, _______, _______, _______, _______, _______, KC_MENU
+                 _______, _______, _______, _______, _______, _______, _______, _______,
+                 _______, _______, _______, _______, _______, _______, _______, _______
     ),
 
 
@@ -81,39 +81,46 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     * |--------+--------+--------+--------+--------||--------+--------+--------+--------+--------|
     * | SCRHLD | LEFT   | DOWN   | RIGHT  | MOUSE3 || TAB    | HOME   | PGDN   | END    | BCKSPC |
     * |--------+--------+--------+--------+--------||--------+--------+--------+--------+--------|
-    * | F13    | F14    | F15    | F16    | F17    || F18    | F19    | F20    | F21    | F22    |
+    * | BACK   | FORWRD | SEARCH | STOP   | REFRSH || F11    | F12    | EXEC   | COPY   | PASTE  |
     * '--------+--------+--------+--------+--------||--------+--------+--------+--------+--------'
-    *          | F23    |        |        |        ||        |        |        | F24    |
+    *          |        |        |        |        ||        |        |        |        |
     *          |--------+--------+--------+--------||--------+--------+--------+--------|
-    *          | MOUSE3 |        |        |        ||        |        |        | END    |
+    *          | MOUSE3 |        |        |        ||        |        |        |        |
     *          '-----------------------------------''-----------------------------------'
     */
     [_FN] = LAYOUT( /* Function Layer, primary alternative layer featuring numpad on right hand,
                        cursor keys on left hand, and all symbols*/
-        XXXXXXX, PLPYTOG, KC_UP,   KC_BTN1, KC_BTN2, KC_ENT,  KC_SPC, KC_PGUP, KC_INS,  KC_DEL,
+        XXXXXXX, PLPYTOG, KC_UP,   KC_BTN1, KC_BTN2, KC_ENT,  KC_SPC,  KC_PGUP, KC_INS,  KC_DEL,
         PLPYHLD, KC_LEFT, KC_DOWN, KC_RGHT, KC_BTN3, KC_TAB,  KC_HOME, KC_PGDN, KC_END,  KC_BSPC,
-        KC_F13,  KC_F14,  KC_F15,  KC_F16,  KC_F17,  KC_F18,  KC_F19,  KC_F20,  KC_F21,  KC_F22,
-                 KC_F23,  _______, _______, _______, _______, _______, _______, KC_F24,
-                 KC_BTN3, _______, _______, _______, _______, _______, _______, KC_EXEC
+        KC_WBAK, KC_WFWD, KC_WSCH, KC_WSTP, KC_WREF, KC_F11,  KC_F12,  KC_EXEC, KC_COPY, KC_PSTE,
+                 _______, _______, _______, _______, _______, _______, _______, _______,
+                 KC_BTN3, _______, _______, _______, _______, _______, _______, _______
     ),
 
-/* blank key layout template
- * .--------------------------------------------..--------------------------------------------.
- * |        |        |        |        |        ||        |        |        |        |        |
- * |--------+--------+--------+--------+--------||--------+--------+--------+--------+--------|
- * |        |        |        |        |        ||        |        |        |        |        |
- * |--------+--------+--------+--------+--------||--------+--------+--------+--------+--------|
- * |        |        |        |        |        ||        |        |        |        |        |
- * '--------+--------+--------+--------+--------||--------+--------+--------+--------+--------'
- *          |        |        |        |        ||        |        |        |        |
- *          |--------+--------+--------+--------||--------+--------+--------+--------|
- *          |        |        |        |        ||        |        |        |        |
- *          '-----------------------------------''-----------------------------------'
- */
+   /* blank key layout template
+    * .--------------------------------------------..--------------------------------------------.
+    * |        |        |        |        |        ||        |        |        |        |        |
+    * |--------+--------+--------+--------+--------||--------+--------+--------+--------+--------|
+    * |        |        |        |        |        ||        |        |        |        |        |
+    * |--------+--------+--------+--------+--------||--------+--------+--------+--------+--------|
+    * |        |        |        |        |        ||        |        |        |        |        |
+    * '--------+--------+--------+--------+--------||--------+--------+--------+--------+--------'
+    *          |        |        |        |        ||        |        |        |        |
+    *          |--------+--------+--------+--------||--------+--------+--------+--------|
+    *          |        |        |        |        ||        |        |        |        |
+    *          '-----------------------------------''-----------------------------------'
+    */
+    [_MACRO] = LAYOUT( /* Macro layer when holding both FN and NUM layer keys together */
+        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+                 XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+                 XXXXXXX, XXXXXXX, XXXXXXX, _______, _______, XXXXXXX, XXXXXXX, XXXXXXX
+    ),
 };
 
 void matrix_scan_user(void) {
-    uint8_t layer = biton32(layer_state);
+    uint8_t layer = get_highest_layer(layer_state);
 
     switch (layer) {
         case _STD:
@@ -124,6 +131,9 @@ void matrix_scan_user(void) {
             break;
         case _NUM:
             set_led_red;
+            break;
+        case _MACRO:
+            set_led_magenta;
             break;
         default:
             break;
@@ -143,4 +153,8 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             break;
     }
     return true;
+}
+
+layer_state_t layer_state_set_user(layer_state_t state) {
+    return update_tri_layer_state(state, _FN, _NUM, _MACRO);
 }
